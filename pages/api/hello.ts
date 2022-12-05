520c742851a8b6cf257893ffa74c7cc676b5ae27
Unidade <68622249+Unidade@users.noreply.github.com>
2022-12-05 14:53:57 -0300

    Add api error

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+const throwError = (req: NextApiRequest, res: NextApiResponse) => {
+  throw new Error('API throw error test')
   res.status(200).json({ name: 'John Doe' })
 }
+export default throwError
