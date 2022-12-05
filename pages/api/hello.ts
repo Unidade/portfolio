7bf2fd169d01f2828c1f98df06d6697189c88454
Unidade <68622249+Unidade@users.noreply.github.com>
2022-12-05 15:03:42 -0300

    Remove Sentry Integration

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const throwError = (req: NextApiRequest, res: NextApiResponse) => {
-  throw new Error('API throw error test')
-  res.status(200).json({ name: 'John Doe' })
-}
-export default throwError
