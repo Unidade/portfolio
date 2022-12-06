0e51e2be8d329c638a6599dc02a9c77b60114c70
Unidade <68622249+Unidade@users.noreply.github.com>
2022-12-06 17:26:54 -0300

    Add DrawerSide component

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,28 +1,17 @@
-import Link from 'next/link'
+import DrawerSide from './DrawerSide'
 import Navbar from './Navbar'
 
 export default function Drawer({ children }: { children: React.ReactNode }) {
   return (
-    <div className='drawer drawer-end'>
-      <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
-      <div className='drawer-content flex px-5 flex-col scroll-smooth'>
-        <Navbar />
-        {children}
+    <>
+      <div className='drawer drawer-end'>
+        <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
+        <div className='drawer-content flex px-5 flex-col scroll-smooth'>
+          <Navbar />
+          {children}
+        </div>
       </div>
-      <div className='drawer-side'>
-        <label htmlFor='my-drawer-3' className='drawer-overlay'></label>
-        <ul className='menu p-4 w-80 bg-base-100'>
-          <li>
-            <Link href='#projetos'>Projetos</Link>
-          </li>
-          <li>
-            <Link href='#curriculo'>Currículo</Link>
-          </li>
-          <li>
-            <Link href='#contato'>Contato</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+      <DrawerSide />
+    </>
   )
 }
