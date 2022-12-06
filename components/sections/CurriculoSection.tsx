21393af092287953887ee2e81964ecb9400e28c0
Unidade <68622249+Unidade@users.noreply.github.com>
2022-12-06 17:29:21 -0300

    Add content on CurriculoSection

--- components/sections/CurriculoSection.tsx
+++ components/sections/CurriculoSection.tsx
@@ -1,3 +1,12 @@
 export default function CurriculoSection() {
-  return <section className=''></section>
+  return (
+    <section className='mt-10'>
+      <h2>Meus certificados</h2>
+      <ul>
+        <li>MongoDB Associate Developer []</li>
+        <li>Postman Expert Student</li>
+        <li>AWS cloud essentials</li>
+      </ul>
+    </section>
+  )
 }
