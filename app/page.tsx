c1cfb2a3adce9a11033d23530182a7531bd187ae
Unidade <68622249+Unidade@users.noreply.github.com>
2022-12-06 23:21:52 -0300

    Stop using app directory, now use pages folder - Delete app folder - Delete next.config.js [unnecessary] - Fix DrawerSide bug - Create pages folder structure

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectsSection from '../components/sections/ProjectsSection'
-
-export default function Home() {
-  return (
-    <main className='mt-10'>
-      <ProjectsSection />
-    </main>
-  )
-}
--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -3,15 +3,13 @@ import Navbar from './Navbar'
 
 export default function Drawer({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div className='drawer drawer-end'>
-        <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
-        <div className='drawer-content flex px-5 flex-col scroll-smooth'>
-          <Navbar />
-          {children}
-        </div>
+    <div className='drawer drawer-end'>
+      <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
+      <div className='drawer-content flex px-5 flex-col scroll-smooth'>
+        <Navbar />
+        {children}
       </div>
       <DrawerSide />
-    </>
+    </div>
   )
 }
