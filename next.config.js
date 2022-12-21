3dcd4778986dc65643aa993b739d4e329d0e628d
Unidade <dario.rodri123@gmail.com>
2022-12-21 20:16:54 -0300

    Change nextConfig.js

--- next.config.js
+++ next.config.js
@@ -2,23 +2,8 @@
  * @type {import('next').NextConfig}
  */
 
-// Github Pages config
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   // Necessary to build on GitHub Pages
-  assetPrefix: assetPrefix ? assetPrefix : undefined,
-  basePath: basePath,
   images: {
     unoptimized: true,
   },
