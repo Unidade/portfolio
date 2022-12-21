10e14f5f76471f2a2738b8e37ae7d1e255fbd7cf
Unidade <dario.rodri123@gmail.com>
2022-12-21 19:08:11 -0300

    Fix npx next export

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  // Necessary to build on GitHub Pages
+  basePath: '/Unidade.github.io',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
