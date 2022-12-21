cedc6e6aefbc105eb451b4322d3a9fd65443f8a6
Unidade <dario.rodri123@gmail.com>
2022-12-21 19:21:21 -0300

    Edit nextjs workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+// Github Pages config
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   // Necessary to build on GitHub Pages
-  basePath: '/Unidade.github.io',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
